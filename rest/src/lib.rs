71cf3fa3b72b2febef3898eecb493267c0277033
Ross Saunders <rosssaunders@outlook.com>
2025-07-05 15:09:15 +0100

    clean up

--- rest/src/lib.rs
+++ rest/src/lib.rs
@@ -8,17 +8,3 @@ pub mod secrets;
 
 // Re-export the main client
 pub use client::Client;
-
-/// Trait for Bitget API requests
-pub trait BitgetRequest: Send + Sync + std::fmt::Debug {
-    type Response: Send + Sync + std::fmt::Debug;
-    
-    /// Get the API endpoint path
-    fn path(&self) -> String;
-    
-    /// Get the HTTP method (GET, POST, etc.)
-    fn method(&self) -> String;
-    
-    /// Whether this request needs authentication/signature
-    fn need_signature(&self) -> bool;
-}
